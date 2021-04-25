33320dc83d20e3868fb30aef377b5427b95b7719
dannyb94 <danielle.ussery@bryanuniversity.edu>
2021-04-25 14:39:13 -0700

    week2

---
+++ week2/stylized-list/dom.js
@@ -0,0 +1,144 @@
+var style = document.createElement('style');
+style.innerHTML = `ul{
+    font-family:sans-serif;
+    color: lightslategray;
+    list-style-type: circle;
+}
+#container{
+    box-shadow: 7px 5px 5px lightgray;
+    margin: 5px;
+    padding: 5px;
+    width: 400px;
+    border-radius: 5px;
+    background-color: #f3f3f3;
+    margin: auto;
+}`;
+document.head.appendChild(style);
+
+var dataList = [
+    {
+        "id": 1,
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Male",
+        "origin": {
+            "name": "Earth (C-137)",
+            "url": "https://rickandmortyapi.com/api/location/1"
+        },
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        
+        "url": "https://rickandmortyapi.com/api/character/1",
+        "created": "2017-11-04T18:48:46.250Z"
+    },
+    {
+        "id": 2,
+        "name": "Morty Smith",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Male",
+        "origin": {
+            "name": "Earth (C-137)",
+            "url": "https://rickandmortyapi.com/api/location/1"
+        },
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        
+        "url": "https://rickandmortyapi.com/api/character/2",
+        "created": "2017-11-04T18:50:21.651Z"
+    },
+    {
+        "id": 3,
+        "name": "Summer Smith",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Female",
+        "origin": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        
+        "url": "https://rickandmortyapi.com/api/character/3",
+        "created": "2017-11-04T19:09:56.428Z"
+    },
+    {
+        "id": 4,
+        "name": "Beth Smith",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Female",
+        "origin": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/4",
+        "created": "2017-11-04T19:22:43.665Z"
+    },
+    {
+        "id": 5,
+        "name": "Jerry Smith",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Male",
+        "origin": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "location": {
+            "name": "Earth (Replacement Dimension)",
+            "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        "url": "https://rickandmortyapi.com/api/character/5",
+        "created": "2017-11-04T19:26:56.301Z"
+    },
+    {
+        "id": 6,
+        "name": "Abadango Cluster Princess",
+        "status": "Alive",
+        "species": "Alien",
+        "type": "",
+        "gender": "Female",
+        "origin": {
+            "name": "Abadango",
+            "url": "https://rickandmortyapi.com/api/location/2"
+        },
+        "location": {
+            "name": "Abadango",
+            "url": "https://rickandmortyapi.com/api/location/2"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+        "episode": [
+            "https://rickandmortyapi.com/api/episode/27"
+        ],
+        "url": "https://rickandmortyapi.com/api/character/6",
+        "created": "2017-11-04T19:50:28.250Z"
+    }
+]
+
+var styledList = document.getElementById('R&M');
+for (var i = 0; i < dataList.length; i++){
+    styledList.innerHTML += '<li>' + `${dataList[i].name}, ${dataList[i].status}, ${dataList[i].species}, ${dataList[i].gender}` + '</li>'
+}
